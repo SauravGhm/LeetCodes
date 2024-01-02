1c6347268935f703e228f798bf0481926b18b012
Saurav <sauravvgh@gmail.com>
2024-01-01 19:26:44 -0500

    multiple lines of for loop has been changed to one

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
 list1 = [3,4,5,6,76,8,9]
 list2 = [3,5,4,2,1,6,7]
-list3 =[]
+
 """while(i<len(list1 and list2)):
     j = list1[i]+list2[i]
     print(j)
@@ -8,9 +8,14 @@ list3 =[]
 
 
 """
+list3 =[]
 for i in range(len(list1 and list2)):
     list3.append(list1[i] + list2[i])
 print(list3)
+
+list4 = [list1[i] + list2[i] for i in range(len(list1 and list2))]
+print("list4", list4)
+
 
 
 
