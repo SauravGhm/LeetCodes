c6ebdfcbefda4daf2edfee4c81b00799f59707cc
Saurav <sauravvgh@gmail.com>
2024-01-01 19:22:11 -0500

    fourth

--- practice.py
+++ practice.py
@@ -1,11 +1,18 @@
 list1 = [3,4,5,6,76,8,9]
 list2 = [3,5,4,2,1,6,7]
-
-i = 0
-while(i<len(list1 and list2)):
+list3 =[]
+"""while(i<len(list1 and list2)):
     j = list1[i]+list2[i]
     print(j)
-    i+=1
+    i+=1"
+
+
+"""
+for i in range(len(list1 and list2)):
+    list3.append(list1[i] + list2[i])
+print(list3)
+
+
 
 
 
