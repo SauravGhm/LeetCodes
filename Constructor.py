2fd3908817ba38f80b2630133be7300802f92eca
Saurav <sauravvgh@gmail.com>
2024-01-11 13:58:45 -0600

    multiple lines of for loop has been changed to one

--- Constructor.py
+++ Constructor.py
@@ -1,19 +1,39 @@
-import datetime
+class ListNode: #defines listnode which represents a node in linked list
+    def __init__(self, data = 0, next_node = None): #constructor for the listnode class,
+        self.data = data
+        self.next = next_node #sets the next attribute of the Listnode instance to the provided next node
 
-class Car:
-    def __init__(self, model, make):
-        self.model = model
-        self.make  = make
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        new_node = ListNode(data)
+        if self.head is None: #Checks if the linked list is empty
+            self.head = new_node #If the Linked List is empty, sets the head of the linked list to the new node
+            return #Exits the method if the linked list was empty
+        last_node = self.head #Initializes a variable 'last_node' to the head of the linked list
+        while last_node.next is not None: #Iterates through the linked list until it reaches the last node-->
+            #-->where next is none
+            last_node = last_node.next #updates 'last_node' to the next node in the linked list
+        last_node.next = new_node #Links the new node to the last node, effectively appending it to the end of
+        #the linked list
 
-class Hoverboard:
-    def __init__(self, flyskill, jumprate):
-        self.flyskill  =  flyskill
-        self.jumprate = jumprate
+    def display(self): #This method is used to display the elements of the linked list
+        current_node = self.head #initializes the variable "current_node" to the head of the linked list
+        while current_node is not None: #Iterates through the linked list
+            print(current_node.data, end ="-->") #prints data of the current node followed by "-->"
+            current_node =current_node.next #updates 'current_node' to the next node in the linked list
+        print("None")
 
-#creating objects by initializing with the class names
+linked_list = LinkedList()
 
-FastFly = Hoverboard("excellent", 100 )
-FastCars = Car("lATEST", 600)
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
 
-print(FastCars.model)
-print(FastFly.jumprate)
+linked_list.display()
+
+
+
+
+
